568d2075f07acd37b9aa4b47c8f56850371277e1
lpreimesberger <meathead123>
2019-12-24 13:11:56 -0600

    new functions and docs

--- tests/visasession_test.py
+++ tests/visasession_test.py
@@ -27,6 +27,15 @@ class MyTestCase(unittest.TestCase):
         print(output)
         self.assertTrue(output.status_code, 200)
 
+
+    def test_dps_get_card_details_(self):
+        print("Expect a 200")
+        my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
+                                 client_cert="../cert.pem", client_key="../rosarius.pem")
+        output = dps_get_card_details(my_session, "7a353971-l4uo-9877-algd-lz1fe25349i9")
+        print(output)
+        self.assertIsNot(output, {})
+
     def test_getbalance_XXXXXXXXXXXX0022(self):
         print("Expect a 200")
         my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
@@ -35,32 +44,42 @@ class MyTestCase(unittest.TestCase):
         print(output)
         self.assertIsNot(output, {})
 
-    def test_getbalance_XXXXXXXXXXXX0037(self):
+
+    def test_gettransactions(self):
         print("Expect a 200")
         my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
                                  client_cert="../cert.pem", client_key="../rosarius.pem")
-        output = dps_get_balances(my_session, "7a353971-l4uo-9877-algd-lz1fe25349i9")
+        output = dps_get_transactions(my_session, "7a353971-l4uo-9877-algd-bp0df3321ly2")
         print(output)
-        self.assertIsNot(output, {})
+        self.assertIsNot(output, [])
+
+    """
+        def test_getbalance_XXXXXXXXXXXX0037(self):
+            print("Expect a 200")
+            my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
+                                     client_cert="../cert.pem", client_key="../rosarius.pem")
+            output = dps_get_balances(my_session, "7a353971-l4uo-9877-algd-lz1fe25349i9")
+            print(output)
+            self.assertIsNot(output, {})
 
 
-    def test_getbalance_XXXXXXXXXXXX0046(self):
-        print("Expect a 200")
-        my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
-                                 client_cert="../cert.pem", client_key="../rosarius.pem")
-        output = dps_get_balances(my_session, "7a353971-l4uo-9877-algd-bd3zl43218j9")
-        print(output)
-        self.assertIsNot(output, {})
+        def test_getbalance_XXXXXXXXXXXX0046(self):
+            print("Expect a 200")
+            my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
+                                     client_cert="../cert.pem", client_key="../rosarius.pem")
+            output = dps_get_balances(my_session, "7a353971-l4uo-9877-algd-bd3zl43218j9")
+            print(output)
+            self.assertIsNot(output, {})
 
 
-    def test_getbalance_XXXXXXXXXXXX0016(self):
-        print("Expect a 200")
-        my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
-                                 client_cert="../cert.pem", client_key="../rosarius.pem")
-        output = dps_get_balances(my_session, "8d212293-c6bc-4738-afaf-bc0ae5456df5")
-        print(output)
-        self.assertIsNot(output, {})
-
+        def test_getbalance_XXXXXXXXXXXX0016(self):
+            print("Expect a 200")
+            my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
+                                     client_cert="../cert.pem", client_key="../rosarius.pem")
+            output = dps_get_balances(my_session, "8d212293-c6bc-4738-afaf-bc0ae5456df5")
+            print(output)
+            self.assertIsNot(output, {})
+    """
 
     def test_getcardid_4883836336860016(self):
         print("Expect a 200 and a valid string of some sort")
@@ -71,32 +90,33 @@ class MyTestCase(unittest.TestCase):
         self.assertIsNot(output, "")
 
 
-    def test_getcardid_4169334953890037(self):
-        print("Expect a 200 and a valid string of some sort")
-        my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
-                                 client_cert="../cert.pem", client_key="../rosarius.pem")
-        output = dps_getcardid(my_session, "4169334953890037")
-        print(output)
-        self.assertIsNot(output, "")
+    """
+        def test_getcardid_4169334953890037(self):
+            print("Expect a 200 and a valid string of some sort")
+            my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
+                                     client_cert="../cert.pem", client_key="../rosarius.pem")
+            output = dps_getcardid(my_session, "4169334953890037")
+            print(output)
+            self.assertIsNot(output, "")
 
 
-    def test_getcardid_4105837613490022(self):
-        print("Expect a 200 and a valid string of some sort")
-        my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
-                                 client_cert="../cert.pem", client_key="../rosarius.pem")
-        output = dps_getcardid(my_session, "4105837613490022")
-        print(output)
-        self.assertIsNot(output, "")
+        def test_getcardid_4105837613490022(self):
+            print("Expect a 200 and a valid string of some sort")
+            my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
+                                     client_cert="../cert.pem", client_key="../rosarius.pem")
+            output = dps_getcardid(my_session, "4105837613490022")
+            print(output)
+            self.assertIsNot(output, "")
 
 
-    def test_getcardid_4386624808860046(self):
-        print("Expect a 200 and a valid string of some sort")
-        my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
-                                 client_cert="../cert.pem", client_key="../rosarius.pem")
-        output = dps_getcardid(my_session, "4386624808860046")
-        print(output)
-        self.assertIsNot(output, "")
-
+        def test_getcardid_4386624808860046(self):
+            print("Expect a 200 and a valid string of some sort")
+            my_session = VisaSession(Config.username, Config.password, be_noisy=True, use_sandbox=True,
+                                     client_cert="../cert.pem", client_key="../rosarius.pem")
+            output = dps_getcardid(my_session, "4386624808860046")
+            print(output)
+            self.assertIsNot(output, "")
+    """
 
 if __name__ == '__main__':
     unittest.main()
--- visa_dps/VisaDPS.py
+++ visa_dps/VisaDPS.py
@@ -7,6 +7,7 @@ Visa DPS Library
 import json
 import logging
 import base64
+from requests import Response
 from visa_dps.VisaSession import VisaSession
 from datetime import datetime
 from typing import List
@@ -50,7 +51,29 @@ class Card:
         self.accounts = accounts
 
 
-def dps_get_balances( session: VisaSession, card_id: str):
+def _unpack_account(output: Response):
+    """Internal function to unpack the struct returned by most GET calls for accounts
+
+       Args:
+            output: Response from VisaSession
+
+        Returns:
+            Account info or {}
+
+        Raises:
+            None    """
+    if output.status_code is not 200:
+        print("Failed %s" % output.url)
+        return {}
+    # this is a Card object above that is wrapped in another junk object
+    account = json.loads(output.text)
+    try:
+        return account['resource']['accounts'][0]
+    except KeyError:
+        return {}
+
+
+def dps_get_balances(session: VisaSession, card_id: str):
     """Get balances for given card_id
 
        Args:
@@ -62,16 +85,23 @@ def dps_get_balances( session: VisaSession, card_id: str):
 
         Raises:
             None    """
-    output = session.get("/dcas/cardservices/v1/cards/%s/accounts?lookUpBalances=true" % card_id)
-    if output.status_code is not 200:
-        print("Failed to get balance info for %s" % card_id)
-        return {}
-    # this is a Card object above that is wrapped in another junk object
-    account = json.loads(output.text)
-    try:
-        return account['resource']['accounts'][0]
-    except KeyError:
-        return {}
+    return _unpack_account(session.get("/dcas/cardservices/v1/cards/%s/accounts?lookUpBalances=true" % card_id))
+
+
+def dps_get_card_details(session: VisaSession, card_id: str):
+    """Get details for given card_id
+
+       Args:
+            session: initialized VisaSession
+            card_id: card_id to get info on, this is _NOT_ a card number (4242 4242 4242 4242)
+
+        Returns:
+            Account info or {}
+
+        Raises:
+            None    """
+    url_packed = "/dcas/cardservices/v1/cards/%s" % card_id
+    return _unpack_account(session.get(url_packed))
 
 
 def dps_cardstatus(session: VisaSession, card_id: str):
@@ -120,3 +150,76 @@ def dps_getcardid(session: VisaSession, card_number: str):
     except KeyError:
         return ""
 
+
+def dps_set_status(session: VisaSession, card_id: str, active_state: bool):
+    """Documentation is crummy for this - Active to enable, Inactive to not?
+
+       Args:
+            session: initialized VisaSession
+            card_id: card id from mapping
+            active_state: turn off or on
+
+        Returns:
+            Text card_id if available, "" else.  API allows multiple but that's weird.
+
+        Raises:
+            None    """
+    mode = "Active"
+    if active_state is not True:
+        mode = "Inactive"
+    pan = {
+            "cardStatus": mode
+        }
+
+    output = session.post("/dcas/cardservices/v1/cards/%s/cardstatus" % card_id, pan)
+    if output.status_code is not 200:
+        print("Whoops")
+        print(output)
+        return ""
+    # this is a Card object above that is wrapped in another junk object
+    card = json.loads(output.text)
+    try:
+        return card['resource']['cards'][0]['cardId']
+    except KeyError:
+        return ""
+
+
+def dps_get_transactions(session: VisaSession, card_id: str, page=None, rows_on_page=10, alias_id=None):
+    """Get transactions for the card (slow, talky)
+
+       Args:
+            session: initialized VisaSession
+            card_id: card id from mapping function
+            page: page of results
+            rows_on_page: results per page (default 10)
+            alias_id: card alias for subquery only
+
+        Returns:
+            array of Transactions or []
+
+        Raises:
+            None    """
+    url_text = "/dcas/cardservices/v1/cards/%s/transactions?rowsOnPage=%s" % (card_id, rows_on_page)
+    if page is not None:
+        url_text = url_text + "&indexRow=" + page
+    if alias_id is not None:
+        url_text = url_text + "&accountAliasId=" + alias_id
+    output = session.get(url_text)
+    if output.status_code is not 200:
+        print("Whoops")
+        print(output)
+        return []
+    # this is a Card object above that is wrapped in another junk object
+    card = json.loads(output.text)
+    try:
+        return card['resource']['transactions']
+    except KeyError:
+        return []
+
+
+# i dont need these - if anyone else really wants we can do.  mobile already know who they are
+# not implementing https://sandbox.api.visa.com/dcas/cardservices/v1/cards/{cardId}/cardverification
+# not implementing https://sandbox.api.visa.com/dcas/cardservices/v1/cards/{cardId}/cardholderdetails
+# doesnt seem useful (dont understand use case - issuers?)
+# https://sandbox.api.visa.com/dcas/cardservices/v1/cards/{cardId}/cardholderdetails
+# https://sandbox.api.visa.com/dcas/cardservices/v1/cards/{cardId}/notifications/otpverification
